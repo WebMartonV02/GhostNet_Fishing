97e84ff1aee6f1e2f9815fe80e8c502f50423c8e
marcellusz <martonvprog@gmail.com>
2023-05-23 23:06:54 +0200

    generic repo with automatic includement, and get all ghsot net for a table

---
+++ GhostNetFishing/aspnet-core/src/GhostNetFishing.Application/GhostNets/GhostNetAppService.cs
@@ -0,0 +1,26 @@
+﻿using GhostNetFishing.Repositories.Common.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace GhostNetFishing.GhostNets
+{
+    public class GhostNetAppService : ApplicationService
+    {
+        private readonly IDefaultRepository<GhostNet> _defaultRepository;
+        public GhostNetAppService(IDefaultRepository<GhostNet> defaultRepository)
+        {
+            _defaultRepository = defaultRepository;
+        }
+
+        public async Task<List<GhostNet>> GetAllAsync()
+        {
+            var result = (await _defaultRepository.GetListWithNestedsAsync()).ToList();
+
+            //use objectmapper and a dto
+
+            return result;
+        }
+    }
+}
---
+++ GhostNetFishing/aspnet-core/src/GhostNetFishing.EntityFrameworkCore/Repositories/Common/DefaultRepository.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using GhostNetFishing.EntityFrameworkCore;
+using GhostNetFishing.Repositories.Common.Interfaces;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace GhostNetFishing.Repositories.Common
+{
+    public class DefaultRepository<TEntity> : EfCoreRepository<GhostNetFishingDbContext,TEntity>, IDefaultRepository<TEntity> where TEntity : class, IEntity<Guid>
+    {
+        private readonly IMapper _mapper;
+
+        public DefaultRepository(
+            IMapper mapper,
+            IDbContextProvider<GhostNetFishingDbContext> dbContextProvider) : base(dbContextProvider)
+        {
+                _mapper = mapper;
+        }
+
+        public async Task<IQueryable<TEntity>> GetListWithNestedsAsync()
+        {
+            var dbSet = await GetDbContextAsync();
+
+            var result = dbSet.Set<TEntity>()
+                .ProjectTo<TEntity>(_mapper.ConfigurationProvider);
+
+            return result;
+        }
+    }
+}
